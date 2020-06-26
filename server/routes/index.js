b483db056da394be57964df1d39eb8ca45b84925
Leticia Martinez <moorenita1202@gmail.com>
2020-06-26 09:07:38 -0500

    Se agrego el catalogo de carreras y epecialidades

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use('/Users',require('./Users'));
-app.use('/Users', require('./Login'));
+app.use(require('./Login'));
 app.use('/Alerts',require('./Alerts'));
 app.use('/Roles',require('./Roles'));
 app.use('/Comments',require('./Comments'));
 app.use('/CategoriaApi',require('./CategoriaApi'));
 app.use('/Api',require('./Api'));
+app.use('/carreras',require('./carreras'));
+app.use('/especialidad',require('./especialidad'));
 
 module.exports = app;
