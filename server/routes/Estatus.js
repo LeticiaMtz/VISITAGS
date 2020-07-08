503f22931b8163d8fe685b8d8d46da12be089e08
Abraham Carranza <aj1998-@hotmail.com>
2020-07-08 14:16:46 -0500

    AA-010 Se reorganizo el codigo de las APIS de los Estatus

--- server/routes/Estatus.js
+++ server/routes/Estatus.js
@@ -8,59 +8,55 @@ const app = express();
 
 
 app.get('/obtener', (req, res) => {
-
-    Estatus.find() //select * from usuario where estado=true
-        //solo aceptan valores numericos
-        .exec((err, estatus) => { //ejecuta la funcion
-            if (err) {
-                return res.status(400).json({
-                    ok: false,
-                    msg: 'error al generar la lista',
-                    resp: err
-                });
-            }
-            console.log(req.estatus);
-            return res.status(200).json({
-                ok: true,
-                msg: 'Lista generada exiosamente',
-                count: estatus.length,
-                resp: estatus
+    Estatus.find().exec((err, estatus) => { //ejecuta la funcion
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'error al generar la lista',
+                resp: err
             });
+        }
+        console.log(req.estatus);
+        return res.status(200).json({
+            ok: true,
+            msg: 'Lista generada exiosamente',
+            count: estatus.length,
+            resp: estatus
         });
+    });
 });
-//Obtener una categoria de crde por id 
+
 app.get('/obtener/:id', (req, res) => {
     let id = req.params.id;
-    Estatus.find({ _id: id })
-        .exec((err, estatus) => {
-            if (err) {
-                return res.status(400).json({
-                    ok: false,
-                    status: 400,
-                    msg: 'Ocurrio un error al consultar los estatus',
-                    cnt: err
-                });
-            }
-            return res.status(200).json({
-                ok: true,
-                status: 200,
-                msg: 'Se han consultado correctamente los estatus',
-                cont: estatus.length,
-                cnt: estatus
+
+    Estatus.find({ _id: id }).exec((err, estatus) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                status: 400,
+                msg: 'Ocurrio un error al consultar los estatus',
+                cnt: err
             });
+        }
+        return res.status(200).json({
+            ok: true,
+            status: 200,
+            msg: 'Se han consultado correctamente los estatus',
+            cont: estatus.length,
+            cnt: estatus
         });
+    });
 });
 
 // Registrar una categoria de crde
 app.post('/registrar', async(req, res) => {
     let body = req.body;
-    //para poder mandar los datos a la coleccion
+
     let estatus = new Estatus({
         strNombre: body.strNombre,
         strDescripcion: body.strDescripcion,
         blnActivo: body.blnActivo,
     });
-
 
     Estatus.findOne({ 'strNombre': body.strNombre }).then((encontrado) => {
         if (encontrado) {
@@ -70,6 +66,7 @@ app.post('/registrar', async(req, res) => {
                 msg: 'El nombre del estatus ya ha sido registrado',
             });
         }
+
         estatus.save((err, estatus) => {
             if (err) {
                 return res.status(400).json({
@@ -92,9 +89,11 @@ app.post('/registrar', async(req, res) => {
 
 app.put('/actualizar/:idEstatus', (req, res) => {
     let id = req.params.idEstatus;
-    console.log(req.params.idEstatus)
+    console.log(req.params.idEstatus);
     const estatusBody = _.pick(req.body, ['strNombre', 'blnActivo', 'strDescripcion']);
+
     Estatus.find({ _id: id }).then((resp) => {
+
         if (resp.length > 0) {
             Estatus.findByIdAndUpdate(id, estatusBody).then((resp) => {
                 return res.status(200).json({
@@ -111,12 +110,15 @@ app.put('/actualizar/:idEstatus', (req, res) => {
                 });
             });
         }
+
     }).catch((err) => {
+
         return res.status(400).json({
             ok: false,
             msg: 'Error al actualizar',
             err: err
         });
+
     });
 });
 
@@ -139,6 +141,7 @@ app.delete('/eliminar/:idEstatus', (req, res) => {
             msg: 'Se ha eliminado correctamente el estatus',
             cnt: resp
         });
+
     });
 });
 
