34aa6b6e38aef9d411815b94c2cbc7f9dea540e3
Palacios <martin.ibpa@outlook.com>
2020-08-20 18:48:03 -0500

    Se agrego informacion de APIs en descarga de archivos

--- server/routes/descargarArchivo.js
+++ server/routes/descargarArchivo.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+//|---------------Api GET Descargar archivos de seguimiento--------------|
+//| Creada por: Martin Palacios                                          |
+//| Api que descarga archivos de seguimiento                             |
+//| modificada por:                                                      |
+//| Fecha de modificacion:                                               |
+//| cambios:                                                             |
+//| Ruta: http://localhost:3000/api/descargaSeguimiento/:fileName'       |
+//|----------------------------------------------------------------------|
 app.get('/descargaSeguimiento/:fileName', (req, res) => {
     let name = req.params.fileName;
 
@@ -11,6 +19,14 @@ app.get('/descargaSeguimiento/:fileName', (req, res) => {
     res.download(filePath, name);
 });
 
+//|---------------Api GET Descargar archivos de evidencia----------------|
+//| Creada por: Martin Palacios                                          |
+//| Api que descarga archivos de evidencia                               |
+//| modificada por:                                                      |
+//| Fecha de modificacion:                                               |
+//| cambios:                                                             |
+//| Ruta: http://localhost:3000/api/descargaEvidencia/:fileName'         |
+//|----------------------------------------------------------------------|
 app.get('/descargaEvidencia/:fileName', (req, res) => {
     let name = req.params.fileName;
 
