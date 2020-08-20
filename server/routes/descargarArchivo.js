837db4b05a3b3759da0a222ad877e02ef10c3af4
Palacios <martin.ibpa@outlook.com>
2020-08-20 14:25:45 -0500

    Se creo la API de descarga de archivos de evidencia

--- server/routes/descargarArchivo.js
+++ server/routes/descargarArchivo.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/descarga/:fileName', (req, res) => {
+app.get('/descargaSeguimiento/:fileName', (req, res) => {
     let name = req.params.fileName;
 
     let filePath = path.resolve(`${__dirname}../../../uploads/seguimiento/${name}`);
+    console.log(filePath);
+
+    res.download(filePath, name);
+});
+
+app.get('/descargaEvidencia/:fileName', (req, res) => {
+    let name = req.params.fileName;
+
+    let filePath = path.resolve(`${__dirname}../../../uploads/evidencias/${name}`);
     console.log(filePath);
 
     res.download(filePath, name);
